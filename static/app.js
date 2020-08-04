cfc5427bc5a4acb443938abd2f745aa0f31b7f0d
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2020-08-03 17:31:59 -0700

    fixed bug. Used AJAX to collect all cupcake instances and display in a list on the page. Created a form to add a cupcake. Used AJAX to submit a post request for the form and create a new cupcake. The list of cupcakes refreshes each time a new cupcake is created.

--- static/app.js
+++ static/app.js
@@ -1,24 +1,44 @@
 'use strict';
+
 // Collect all cupcake instances from cupcakes api
 async function getCupcakes() {
     let response = await axios.get('/api/cupcakes');
     return response.data;
 }
 
-// Create list elements to display each cupcake instance
-
+// Create linked-list that displays each cupcake instance
 async function listCupcakes() {
-    let response = await getCupcakes();
-    console.log('response', response)
+    $('#cupcake-list').empty();
 
-    // cupcakes = JSON.parse(response.data)
+    let response = await getCupcakes();
+    let cupcakes = response.cupcakes;
+    console.log('response', response.cupcakes);
+
+    for (let cupcake of cupcakes) {
+        let $link = $('<a>').attr('href', `/cupcake/${cupcake.id}`).text(`${cupcake.flavor}`);
+        let $listElement = $('<li>').append($link);
+        $('#cupcake-list').append($listElement);
+    }
 }
 
-getCupcakes();
+$(document).ready(listCupcakes());
 
 
-// $(document).ready(function(){
-//     console.log('ready!');
-// })
+// Creates a new cupcake after form submission
+async function bakeCupcake() {
 
-$(document).ready(listCupcakes);
+    let flavor = $('#flavor').val();
+    let size = $('#size').val();
+    let rating = $('#rating').val();
+    let image = $('#image').val();
+    // console.log('form object', { flavor, size, rating, image });
+
+    await axios.post('/api/cupcakes', { flavor, size, rating, image });
+    await listCupcakes();
+}
+
+// Event handler for backCupcake
+$('#create-cupcake-form').on('click', 'button', function(evt){
+    evt.preventDefault();
+    bakeCupcake();
+})
